c3289a8860b5a14c0a20645d78a05a55b0ce5987
douglas5001 <douglasportella66@gmail.com>
2025-11-18 10:26:14 -0300

    fix: ]remove create_all do main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from app.config.config import config
 from app.utils.logging import setup_logging
-from app.config.database import Base, engine
+# from app.config.database import Base, engine
 from app.controllers import ticket_controller
 
 setup_logging()
 
 # DEV: cria as tabelas que ainda não existem / Ou você pode criar as tabelas pelo Alembic seguindo o RADME
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title=config.app_name)
 
